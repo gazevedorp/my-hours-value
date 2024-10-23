271fd16304b23e1812e62760c64fc5979cd8673f
Gabriel Azevedo <gabrielazevedo@MacBook-Pro-de-Gabriel.local>
2024-10-22 21:17:34 -0300

    chore: add clear button

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,24 +13,31 @@ const geistMono = localFont({
 });
 
 export default function Home() {
-  const [hours, setHours] = useState();
-  const [days, setDays] = useState();
-  const [totalValue, setTotalValue] = useState();
+  const [hours, setHours] = useState(0);
+  const [days, setDays] = useState(0);
+  const [totalValue, setTotalValue] = useState(0);
   const [hourValue, setHourValue] = useState(0);
 
   const handleCalculateHour = (e) => {
     e.preventDefault();
     const totalHours = days * hours;
-    const HourValueTemp = (totalValue / totalHours).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
-    setHourValue(HourValueTemp);
+    const HourValueTemp = totalValue / totalHours
+    setHourValue(HourValueTemp || 0);
+  }
+
+  const handleClear = () => {
+    setHourValue(0);
+    setDays(0);
+    setTotalValue(0);
+    setHourValue(0);
   }
 
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_280px_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <h1 className="text-blue-700 text-xl">CÁLCULO DO VALOR DE HORA</h1>
-      <form className="flex flex-col justify-between items-center" onSubmit={handleCalculateHour}>
+      <form className="flex flex-col justify-between items-center p-0 m-0" onSubmit={handleCalculateHour}>
         <div className="mb-2">
           <label className="block text-sm font-medium text-gray-900 dark:text-white">Horas trabalhadas (Dia)</label>
           <input type="number" value={hours} onChange={(e) => setHours(e.target.value)} id="hours" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
@@ -46,6 +53,9 @@ export default function Home() {
         <div className="w-full">
           <button type="submit" className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
             Calcular
+          </button>
+          <button onClick={() => handleClear()} className="text-white w-full bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
+            Limpar
           </button>
         </div>
       </form>
